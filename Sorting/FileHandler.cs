02bbabd71836b6bcd6cdcec70e6d5a370d11d8a8
DRAGAN DJORDJEVIC <101023448@student.swin.edu.au>
2018-07-01 17:01:13 +1000

    Add project files.

---
+++ Sorting/FileHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting
+{
+      class FileHandler
+      {
+
+            public FileHandler() { }
+
+            public List<string> ReadFile(string filePath)
+            {
+
+                  return File.ReadAllLines(filePath).ToList<string>();
+
+            }
+      }
+}
